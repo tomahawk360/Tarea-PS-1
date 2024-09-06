708084c3c7098965ca75057d2675c2d230a305d0
Felipe Buscaglia <felipe.buscaglia@usm.cl>
2024-09-06 16:54:21 -0400

    agregado fecha inicio

--- tareas.py
+++ tareas.py
@@ -4,23 +4,29 @@ class Tarea:
     def __init__(self, titulo, descripcion, etiqueta):
         self.titulo = titulo
         self.descripcion = descripcion
-        self.fecha_vencimiento = self.ingresar_fecha_vencimiento()  # Llamada a la función durante la creación
+        self.fecha_inicio = self.ingresar_fecha(1)  # Llamada a la función durante la creación
+        self.fecha_vencimiento = self.ingresar_fecha(2)  # Llamada a la función durante la creación
         self.etiqueta = etiqueta
 
-    def ingresar_fecha_vencimiento(self):
+    def ingresar_fecha(self, option):
         while True:
             try:
-                entrada = input("Ingrese la fecha y hora de vencimiento (YYYY-MM-DD HH:MM): ")
-                fecha_vencimiento = datetime.strptime(entrada, '%Y-%m-%d %H:%M')
-                return fecha_vencimiento
+                if option==1:
+                    entrada = input("Ingrese la fecha y hora de inicio (YYYY-MM-DD HH:MM): ")
+                elif option==2:
+                    entrada = input("Ingrese la fecha y hora de vencimiento (YYYY-MM-DD HH:MM): ")
+                fecha = datetime.strptime(entrada, '%Y-%m-%d %H:%M')
+                return fecha
             except ValueError:
                 print("Formato de fecha y hora incorrecto. Por favor, ingrese la fecha en el formato 'YYYY-MM-DD HH:MM'.")
 
     def mostrar_tarea(self):
-        fecha_formateada = self.fecha_vencimiento.strftime('%Y-%m-%d %H:%M')
+        fecha_inicio_formateada = self.fecha_vencimiento.strftime('%Y-%m-%d %H:%M')
+        fecha_vencimiento_formateada = self.fecha_vencimiento.strftime('%Y-%m-%d %H:%M')
         return (f"Título: {self.titulo}\n"
                 f"Descripción: {self.descripcion}\n"
-                f"Fecha de Vencimiento: {fecha_formateada}\n"
+                f"Fecha de Inicio: {fecha_inicio_formateada}\n"
+                f"Fecha de Vencimiento: {fecha_vencimiento_formateada}\n"
                 f"Etiqueta: {self.etiqueta}")
 
 # Crear una instancia de Tarea
